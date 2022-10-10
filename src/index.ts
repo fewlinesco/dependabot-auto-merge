b90acc0414da5522e3b354637b29352e425b4cda
Clement Meyer <meyclem@gmail.com>
2022-10-10 16:06:24 +0200

    add workflow for testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import log from "./func";
+import core from "@actions/core";
+import github from "@actions/github";
 
-log();
+const GITHUB_TOKEN = core.getInput("gh-pat");
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+async function run(): Promise<void> {
+  const { pull_request } = github.context.payload;
+  if (pull_request) {
+    console.log("context", github.context);
+    console.log("repo", github.context.repo);
+
+    await octokit.rest.issues.createComment({
+      ...github.context.repo,
+      issue_number: pull_request.number,
+      body: "This message is a test",
+    });
+  }
+}
+
+run();
